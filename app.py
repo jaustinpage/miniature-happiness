27ae5fb0e91e882439a433d4cc45bd80988f6d69
Austin Page <jaustinpage@gmail.com>
2022-01-25 21:22:04 -0600

    Fix import error

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 """Shim to allow this to work by just executing flask run."""
 
-from src.miniature_happiness import create_app
+import miniature_happiness
+
+app = miniature_happiness.create_app()
 
 if __name__ == "__main__":
-    app = create_app()
     app.run()
